1edf8d19cd7d5beb0ae90d1eb876fa061250ca20
JoseGordilloMendoza <121585572+JoseGordilloMendoza@users.noreply.github.com>
2023-05-06 23:05:36 -0500

    Add files via upload

---
+++ calculadora.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class calculadora{
+    public static void main(String [] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("\t.:Calculadora:.\t");
+        System.out.print("Primer numero: ");
+        int numero1=sc.nextInt();
+        System.out.print("Segundo numero: ");
+        int numero2=sc.nextInt();
+    }
+}
+ 
