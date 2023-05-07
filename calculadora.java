530b5d4e8a8abf29922b82160c301658ab45b695
JoseGordilloMendoza <121585572+JoseGordilloMendoza@users.noreply.github.com>
2023-05-07 00:50:33 -0500

    Update calculadora.java

--- calculadora.java
+++ calculadora.java
@@ -32,11 +32,11 @@ public class calculadora{
 	}
 
     //Creamos el metodo para obtener el modulo entre dos numeros
-    public int mod(int num1, int num2){
+    public String mod(int num1, int num2){
         if (num2==0)
-            return num1%num2;
+            return "La operacion no se puede realizar, no hay division sobre 0";
         else
-            return -1;
+            return num1%num2;
     }
 }
  
