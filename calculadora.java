1620e62ea68bad55dce209eb82f48ed7f4b976f4
JoseGordilloMendoza <121585572+JoseGordilloMendoza@users.noreply.github.com>
2023-05-06 23:06:04 -0500

    BASE DEL PROGRAMA

--- calculadora.java
+++ calculadora.java
@@ -1,6 +1,7 @@
 import java.util.*;
 public class calculadora{
     public static void main(String [] args){
+        //CODIGO BASE DEL PROGRAMA
         Scanner sc=new Scanner(System.in);
         System.out.println("\t.:Calculadora:.\t");
         System.out.print("Primer numero: ");
